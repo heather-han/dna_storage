90536d16b107527739cc681b4edfb1f64b4a48e4
heather-han <heather.h.1122@gmail.com>
2019-04-15 17:01:55 -0400

    runtime scripts

--- runtime.sh
+++ runtime.sh
@@ -13,22 +13,21 @@ for (( i=0;i<${#files[@]};i++ )); do
 
 	# time encoding the file
 	echo "Encoding ${files[i]}..."
-	enc_time="$(time (python encode_h.py ${files[i]}) 2>&1 1>/dev/null )"
+	enc_time="$(time (python encode.py ${files[i]}) 2>&1 1>/dev/null )"
 	enc_times=( ${enc_times[@]} enc_time )
 	echo "Encode ${files[i]}: $enc_time" >> "runtime"
 
 	# time decoding the file
 	echo "Decoding ${files[i]}..."
-	dec_time="$(time (python decode_h.py $filename.dna) 2>&1 1>/dev/null )"
+	dec_time="$(time (python decode.py $filename.dna) 2>&1 1>/dev/null )"
 	dec_times=( ${dec_times[@]} dec_time )
 	echo "Decode ${files[i]}: $dec_time" >> "runtime"
 done
 
 
-# runtimes for zip compressed files
+# # runtimes for zip compressed files
 rm runtime_zip
 touch runtime_zip
-zip_times=()
 enc_times=()
 dec_times=()
 for (( i=0;i<${#files[@]};i++ )); do
@@ -41,13 +40,13 @@ for (( i=0;i<${#files[@]};i++ )); do
 	
 	# time encoding the file
 	echo "Encoding ${files[i]}..."
-	enc_time="$(time (python encode_h.py ${files[i]}.zip) 2>&1 1>/dev/null )"
+	enc_time="$(time (python encode.py ${files[i]}.zip) 2>&1 1>/dev/null )"
 	enc_times=( ${enc_times[@]} enc_time )
 	echo "Encode ${files[i]}: $enc_time" >> "runtime_zip"
 
 	# time decoding the file
 	echo "Decoding ${files[i]}..."
-	dec_time="$(time (python decode_h.py $filename.dna) 2>&1 1>/dev/null )"
+	dec_time="$(time (python decode.py $filename.dna) 2>&1 1>/dev/null )"
 	dec_times=( ${dec_times[@]} dec_time )
 	echo "Decode ${files[i]}: $dec_time" >> "runtime_zip"
 done
@@ -56,7 +55,6 @@ done
 # runtimes for gzip compressed files
 rm runtime_gzip
 touch runtime_gzip
-gzip_times=()
 enc_times=()
 dec_times=()
 for (( i=0;i<${#files[@]};i++ )); do
@@ -69,13 +67,40 @@ for (( i=0;i<${#files[@]};i++ )); do
 	
 	# time encoding the file
 	echo "Encoding ${files[i]}..."
-	enc_time="$(time (python encode_h.py ${files[i]}.gz) 2>&1 1>/dev/null )"
+	enc_time="$(time (python encode.py ${files[i]}.gz) 2>&1 1>/dev/null )"
 	enc_times=( ${enc_times[@]} enc_time )
 	echo "Encode ${files[i]}: $enc_time" >> "runtime_gzip"
 
 	# time decoding the file
 	echo "Decoding ${files[i]}..."
-	dec_time="$(time (python decode_h.py $filename.dna) 2>&1 1>/dev/null )"
+	dec_time="$(time (python decode.py $filename.dna) 2>&1 1>/dev/null )"
 	dec_times=( ${dec_times[@]} dec_time )
 	echo "Decode ${files[i]}: $dec_time" >> "runtime_gzip"
+done
+
+
+# runtimes for 7zip compressed file
+rm runtime_7zip
+touch runtime_7zip
+enc_times=()
+dec_times=()
+for (( i=0;i<${#files[@]};i++ )); do
+	filename=`cut -d "." -f1 <<< ${files[i]}`
+
+	# time zipping the file
+	echo "Zipping ${files[i]}..."
+	zip_time="$(time (7z a ${files[i]}.7z ${files[i]}) 2>&1 1>/dev/null )"
+	echo "Zip ${files[i]}: $zip_time" >> "runtime_7zip"
+	
+	# time encoding the file
+	echo "Encoding ${files[i]}..."
+	enc_time="$(time (python encode.py ${files[i]}.7z) 2>&1 1>/dev/null )"
+	enc_times=( ${enc_times[@]} enc_time )
+	echo "Encode ${files[i]}: $enc_time" >> "runtime_7zip"
+
+	# time decoding the file
+	echo "Decoding ${files[i]}..."
+	dec_time="$(time (python decode.py $filename.dna) 2>&1 1>/dev/null )"
+	dec_times=( ${dec_times[@]} dec_time )
+	echo "Decode ${files[i]}: $dec_time" >> "runtime_7zip"
 done
